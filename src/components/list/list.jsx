b6e1327bdfcd049bf747045f4459b8884101658c
usiyoung <usiyoung7@gmail.com>
2022-01-19 14:50:40 +0900

    add roadmap and styling

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -52,7 +52,25 @@ const List = memo(({onSearch, onTarget, handleUrl}) => {
 
     return(
         <ul className={`${styles.list} `}>
-            {lists.map(list => <li ref={el => (navRef.current[list.id -1] = el)} key={list.id} onClick={onClick} data-url={list.url}className={styles.listItem}>{list.username}</li>)}
+            <li className={styles.roadMap}>
+                <a 
+                href="https://roadmap.sh"
+                target="_blank"
+                rel="noreferrer"
+                >
+                프론트엔드 로드맵
+            </a>
+            </li>
+            {lists.map(
+                list => 
+                <li 
+                ref={el => (navRef.current[list.id -1] = el)} key={list.id} 
+                onClick={onClick} 
+                data-url={list.url} 
+                className={styles.listItem}>
+                    {list.username}
+                </li>
+                )}
         </ul>
     )          
 });
